9a92d7e5ba04bce5405c817cf46e38f6ea88a644
dongju <131073398+Dongjuice@users.noreply.github.com>
2024-09-13 00:25:47 +0900

    캘린더 클리핑 비활성화 처리 및 동적 높이조절 작업완료

--- Group/Presentation/MainScene/SubScene/CalendarScene/View/CalendarViewController.swift
+++ Group/Presentation/MainScene/SubScene/CalendarScene/View/CalendarViewController.swift
@@ -11,7 +11,16 @@ import FSCalendar
 import RxSwift
 import RxCocoa
 
+private enum MonthType: Int {
+    case fourWeekMonth = 4
+    case fiveWeekMonth = 5
+    case sixWeekMonth = 6
+}
+
 final class CalendarViewController: BaseViewController, FSCalendarDelegate, FSCalendarDataSource {
+    
+    private var monthType: MonthType = .fiveWeekMonth
+    private var isWeekView: Bool = false
     
     // MARK: - Test
     var scope: FSCalendarScope {
@@ -27,6 +36,12 @@ final class CalendarViewController: BaseViewController, FSCalendarDelegate, FSCa
     
     private let currentCalendar = Calendar.current
     
+    private let emptyView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemBlue
+        return view
+    }()
+    
     private lazy var today: Date = self.currentCalendar.startOfDay(for: Date())
     
     private var selectedDate: Date?
@@ -39,40 +54,60 @@ final class CalendarViewController: BaseViewController, FSCalendarDelegate, FSCa
         super.viewDidLoad()
         setCalendar()
         setupUI()
-        
         setAction()
     }
     
+    private let containerView = UIView()
+    
     private func setupUI() {
         self.view.addSubview(calendar)
+        self.view.addSubview(emptyView)
         self.view.addSubview(testBtn)
+        calendar.addSubview(containerView)
+        containerView.addSubview(calendar.calendarWeekdayView)
         
-        let calendarHeight = calendar.weekdayHeight + (calendar.rowHeight * 5)
+        let calendarHeight = calendar.weekdayHeight + (calendar.rowHeight * 6)
         
         calendar.snp.makeConstraints { make in
             make.top.equalTo(titleViewBottom)
-            make.horizontalEdges.equalToSuperview().inset(24)
+            make.horizontalEdges.equalToSuperview()
             make.height.equalTo(calendarHeight)
         }
         
-        testBtn.snp.makeConstraints { make in
-            make.top.equalTo(calendar.snp.bottom)
-            make.trailing.equalTo(calendar.snp.trailing)
-            make.size.equalTo(40)
+        containerView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(24)
+            make.height.equalTo(36)
         }
         
-        testBtn.layer.cornerRadius = 20
+        calendar.calendarWeekdayView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        calendar.collectionViewLayout.sectionInsets = .init(top: 5, left: 24, bottom: 5, right: 24)
+                
+        testBtn.snp.makeConstraints { make in
+            make.center.equalTo(emptyView)
+            make.size.equalTo(100)
+        }
+        
+        emptyView.snp.makeConstraints { make in
+            make.top.equalTo(calendar.snp.bottom)
+            make.horizontalEdges.bottom.equalToSuperview()
+        }
     }
     
     private func setCalendar() {
         calendar.delegate = self
         calendar.dataSource = self
+        self.view.clipsToBounds = false
         
         calendar.headerHeight = 0
-        calendar.weekdayHeight = 36
-        calendar.rowHeight = 56
-        
+        calendar.rowHeight = 60
+
         calendar.scrollDirection = .horizontal
+        
+        calendar.adjustsBoundingRectWhenChangingMonths = true
         
         calendar.appearance.weekdayTextColor = UIColor(hexCode: "999999")
         
@@ -98,12 +133,80 @@ final class CalendarViewController: BaseViewController, FSCalendarDelegate, FSCa
                         isToday: checkToday(date))
         return cell
     }
-    
+
     func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        
+        let height = changeHeight(bounds.height)
+                
         calendar.snp.updateConstraints { (make) in
-            make.height.equalTo(bounds.height)
+            make.height.equalTo(height)
         }
+        
         self.view.layoutIfNeeded()
+    }
+    
+    
+    /// 주간 뷰에서 월간 뷰로 변경 시 높이 조절하기
+    /// - Parameter height: FSCalendar 조정 높이
+    /// - Returns: Custom 조정 높이
+    private func changeHeight(_ height: CGFloat) -> CGFloat {
+        guard isWeekView else { return height }
+        
+        // viewType 초기화
+        isWeekView = false
+        
+        return heightOfMonthType()
+    }
+    
+    /// 주 갯수에 따라서 높이 변경
+    private func heightOfMonthType() -> CGFloat {
+        let rowHeight = calendar.rowHeight
+        switch monthType {
+        case .fourWeekMonth:
+            return rowHeight * CGFloat(monthType.rawValue) + 10
+        case .fiveWeekMonth:
+            return rowHeight * CGFloat(monthType.rawValue) + 5
+        case .sixWeekMonth:
+            return rowHeight * CGFloat(monthType.rawValue)
+        }
+    }
+    
+    /// isWeekView와 scope 동기화
+    private func updateIsWeekViewFlag(scope: FSCalendarScope) {
+        isWeekView = scope == .month ? false : true
+    }
+    
+    /// 주간 달력인 경우에는 저장하지 않기 위해 scope가 month인 경우에만 monthType 업데이트
+    private func updateWeekType() {
+        if calendar.scope == .month {
+            let calendarHeight = calendar.frame.height
+            monthType = updateMonthType(height: calendarHeight)
+        }
+    }
+    
+    /// 현재 달력의 높이에 따라서 6주 or 5주로 변경
+    /// - Parameters:
+    ///   - height: 현재 달력의 높이
+    ///   - tolerance: 오차 범위
+    private func updateMonthType(height: CGFloat, tolerance: CGFloat = 20) -> MonthType {
+        let currentHeight = height + tolerance // 현재 높이
+        let sixWeekMonthHeight = calendar.rowHeight * 6
+        let fiveWeeMonthkHeight = calendar.rowHeight * 5
+
+        switch currentHeight {
+        case ..<fiveWeeMonthkHeight:
+            return .fourWeekMonth
+        case ..<sixWeekMonthHeight:
+            return .fiveWeekMonth
+        default:
+            return .sixWeekMonth
+        }
+    }
+
+    // 캘린더 현재 높이 저장
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        updateWeekType()
     }
     
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
@@ -134,9 +237,22 @@ final class CalendarViewController: BaseViewController, FSCalendarDelegate, FSCa
     private func setAction() {
         self.testBtn.rx.controlEvent(.touchUpInside)
             .subscribe(with: self, onNext: { vc, _ in
-                vc.calendar.setScope(self.scope, animated: true)
+                vc.updateIsWeekViewFlag(scope: vc.calendar.scope)
+                vc.calendar.setScope(vc.scope, animated: true)
             })
             .disposed(by: disposeBag)
+    }
+    
+    private func moveToSpecificYearMonth(year: Int, month: Int) {
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.year = year
+        dateComponents.month = month
+        dateComponents.day = 1 // 해당 월의 1일로 설정
+
+        if let date = calendar.date(from: dateComponents) {
+            self.calendar.setCurrentPage(date, animated: true)
+        }
     }
 }
 
@@ -234,5 +350,6 @@ struct CalendarViewController_Preview: PreviewProvider {
     }
 }
 #endif
+
 
 
